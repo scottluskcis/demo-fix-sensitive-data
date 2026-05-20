9d9af9f74c1bb5ffe1f96bbd8046988ac87afa25
Christopher-Lusk <Christopher.Lusk@va.gov>
2026-05-20 15:50:43 -0400

    add example with fake PII

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+persons = [
+    { "name": "Alice", "age": 30, "identifier": "***-**-1123", "identfier_type": "SSN" },
+    { "name": "Bob", "age": 25, "identifier": "***-**-3822", "identfier_type": "SSN" },
+    { "name": "Charlie", "age": 35, "identifier": "***-**-3423", "identfier_type": "SSN" },
+]
+
 def get_message(name):
+    for person in persons:
+        if person["name"].lower() == name.lower().strip():
+            return f"Hello, {person['name']}! Your age is {person['age']} and your {person['identfier_type']} is {person['identifier']}."
+
     return f"Hello, {name}!"
 
 def hello():
